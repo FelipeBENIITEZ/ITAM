8a3836c82d9851020a7b1509c3a0fe6c4ef7efa1
Felipe ING <felipeenciso2013@gmail.com>
2025-07-15 15:49:55 -0300

    Sistema ITAM 1.1

---
+++ src/main/java/com/sistema/iTsystem/ITsystemApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.iTsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/iTsystem/ITsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.iTsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
